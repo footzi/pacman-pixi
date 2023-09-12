efd8717b5a09ae79a646478d54d8f0fc496da0d5
Vladislav Manakov <vsmanakov@avito.ru>
2023-09-12 14:16:36 +0400

    Remove comments

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,6 @@ import { Border } from './Border';
 import { CONSTANTS } from './constants.ts';
 import { Utils } from './Utils';
 
-// const app = new Application({ width: 640, height: 360 });
-// // @ts-ignore
-// document.body.appendChild(app.view);
-//
-// const sprite = Sprite.from('./assets/sample.png');
-// app.stage.addChild(sprite);
-//
-// // Add a variable to count up the seconds our demo has been running
-// let elapsed = 0.0;
-// // Tell our application's ticker to run a new callback every frame, passing
-// // in the amount of time that has passed since the last tick
-// app.ticker.add((delta) => {
-//   // console.log(delta);
-//   // Add the time to our total elapsed time
-//   elapsed += delta;
-//   // Update the sprite's X position based on the cosine of our elapsed time.  We divide
-//   // by 50 to slow the animation down a bit...
-//   sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
-// });
-
 class Game {
   app: Application;
   player: Player;
@@ -51,36 +31,29 @@ class Game {
   update() {
     const playerNextParams = this.player.getNextRight();
 
-    // for (let i = 0; i < this.borders.length; i++) {
-    //   const border = this.borders[i];
-    //   const borderParams = border.getParams();
-    //
-    //   // console.log(this.isRight);
-    //
-    //   if (this.isRight) {
-    //     // this.isRight = false;
-    //     const prevParams = this.player.getPreviousParams();
-    //     const playerParams = this.player.getParams();
-    //     const nextParams = this.player.getNextRight();
-    //
-    //     // console.log(prevParams);
-    //     console.log(playerParams);
-    //     console.log(borderParams, 'border params');
-    //
-    //     if (Utils.isCollision(nextParams, borderParams)) {
-    //       // const { x } = Utils.getCollusionCoordsSafety(nextParams, borderParams);
-    //       // console.log('colisiion');
-    //       // const x = nextParams.x - playerParams.x;
-    //       // this.player.rightOn(x);
-    //       // this.isRight = false;
-    //       // this.isRight = false;
-    //     } else {
-    //       this.player.right();
-    //       this.isRight = false;
-    //     }
-    //   }
-    // }
-    // console.log('update');
+    for (let i = 0; i < this.borders.length; i++) {
+      const border = this.borders[i];
+      const borderParams = border.getParams();
+
+      if (this.isRight) {
+        const prevParams = this.player.getPreviousParams();
+        const playerParams = this.player.getParams();
+        const nextParams = this.player.getNextRight();
+
+        if (Utils.isCollision(nextParams, borderParams)) {
+          const { x } = Utils.getCollusionCoordsSafety(nextParams, borderParams);
+
+          const x = nextParams.x - playerParams.x;
+          this.player.rightOn(x);
+          this.isRight = false;
+          this.isRight = false;
+        } else {
+          this.player.right();
+          this.isRight = false;
+        }
+      }
+    }
+    console.log('update');
   }
 
   initBorders() {
@@ -98,15 +71,6 @@ class Game {
     this.player.render();
 
     let isCollision = false;
-
-    // document.addEventListener('keydown', (event) => {
-    //   const { key } = event;
-    //
-    //   if (key === 'ArrowRight') {
-    //     console.log('keydown');
-    //     this.isRight = true;
-    //   }
-    // });
 
     document.addEventListener('keyup', (event) => {
       const { key } = event;
